3065bf7170112de364fd22a2b7f2b87b71741f2e
Ivan <ivan.holyk369@gmail.com>
2025-04-27 20:19:01 +0300

    fixed url path  did not choose components in router

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createMemoryHistory } from "vue-router"
+import { createRouter, createWebHistory } from "vue-router"
 
 import LineList from "@/view/LineList.vue"
 import StationTimetable from "@/view/StationTimetable.vue"
@@ -10,7 +10,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createMemoryHistory(),
+  history: createWebHistory(),
   routes,
 })
 
