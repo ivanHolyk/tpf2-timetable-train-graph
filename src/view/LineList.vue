c61e172d40a147f65fa1d2a4c424193faac45c1a
Ivan <ivan.holyk369@gmail.com>
2025-04-27 20:20:03 +0300

    add search through lines

--- src/view/LineList.vue
+++ src/view/LineList.vue
@@ -11,8 +11,34 @@
 </template>
 <script setup>
 import { useLineStore } from "@/stores/lines"
+import { useRoute } from "vue-router"
+import { computed } from "vue"
 import LineComponent from "@/components/LineComponent.vue"
+import { useNameStore } from "@/stores/names"
 
 const lineStore = useLineStore()
-const lines = lineStore.getLines()
+const route = useRoute()
+const nameStore = useNameStore()
+
+const lineFilter = computed(() => {
+  const q = route.query.line
+  if (!q) return null
+  const arr = Array.isArray(q) ? q : q.split(",")
+  return arr.map((x) => x.trim()).filter(Boolean)
+})
+
+const lines = computed(() => {
+  const allLines = lineStore.getLines()
+
+  if (!lineFilter.value) return allLines.value
+  return allLines.value.filter((line) => {
+    return (
+      line.id.includes(lineFilter.value) ||
+      nameStore
+        .getName(line.id)
+        .toLowerCase()
+        .includes(lineFilter.value[0].toLowerCase())
+    )
+  })
+})
 </script>
